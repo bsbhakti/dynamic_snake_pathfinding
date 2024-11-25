3cc3d9ef81549e433963aa64d5a7a9d6c46ca79b
bsbhakti <70367592+bsbhakti@users.noreply.github.com>
2024-11-24 18:42:15 -0800

    Lpa star improvements but still buggy

--- lpa_star.py
+++ lpa_star.py
@@ -23,7 +23,8 @@ def lpa_star(problem, start_loc, goal_loc,agent,h_values):
         start_node['priority'] = calculate_key(start_loc)
 
         rhs[start_loc] = 0
-        push_node(open_list,start_node, calculate_key(start_loc),node_map)
+        print("calling push1")
+        push_node(open_list,start_node, calculate_key(start_loc))
 
     def update_vertex(node):
         loc = node["loc"]
@@ -34,19 +35,23 @@ def lpa_star(problem, start_loc, goal_loc,agent,h_values):
             for succ  in next_states:
                 new_gval = gVal[succ]+1
                 if(rhs[succ] < new_gval ):
-                    rhs[node["loc"]] = min(rhs[succ], new_gval)
-                    print("this is node map ", node_map)
-                    node["parent"] = node_map[(succ,node["time"]-1)]
+                    rhs[node["loc"]] = min(rhs[node["loc"]], new_gval)
+                    print("updated rhs of loc ", loc,  rhs[node["loc"]])
+
+
+                    # print("this is node map ", node_map)
+                    # node["parent"] = node_map[(succ,node["time"]-1)]
             # print("updating rhs of loc", node["loc"], m)
             # rhs[node["loc"]] = m
         k = calculate_key(node["loc"])
 
         # if node_map.get((loc, node["time"])):  # If node exisats in the map
-        print("removing node",node)
-        remove_node(open_list, node, node_map)
+        remove_node(open_list, node)
+        print("removing node, new priority",k,node)
         if gVal[loc] != rhs[loc]:
             node["priority"] = k
-            push_node(open_list, node, k, node_map)
+            print("pushing node", node)
+            push_node(open_list, node, k)
 
         # if(k[0], k[1],node["loc"],node in open_list):
         #     print("removing")
@@ -56,7 +61,8 @@ def lpa_star(problem, start_loc, goal_loc,agent,h_values):
         #     push_node(open_list, node, calculate_key(node["loc"]),node_map)
 
     def compute_shortest_path():
-        while top_key(open_list) < calculate_key(goal_loc) or rhs[goal_loc] != gVal[goal_loc]:
+        print("this is goal node's priority",calculate_key(goal_loc))
+        while (top_key(open_list)[0] < calculate_key(goal_loc)[0] and top_key(open_list)[1] < calculate_key(goal_loc)[1])  or rhs[goal_loc] != gVal[goal_loc]:
             node = pop_node(open_list)
             print("node pooped", node)
 
@@ -89,15 +95,7 @@ def lpa_star(problem, start_loc, goal_loc,agent,h_values):
                 update_vertex(node)
 
 
-    def reconstruct_path():
-        path = []
-        loc = goal_loc
-        while loc:
-            path.append(loc)
-            print("d")
-            print(node_map)
-            loc = node_map[loc]["parent"] if loc in node_map else None
-        return path[::-1]
+  
 
 
 
@@ -105,7 +103,7 @@ def lpa_star(problem, start_loc, goal_loc,agent,h_values):
         initialize()
         while True:
             compute_shortest_path()
-            print(reconstruct_path())
+            # print(reconstruct_path())
             # Waitfor changes in edge costs; 
             # {21} forall directed edges (u,v) with changed edge costs 
             # {22} Update the edge cost c(u,v); {23} UpdateVertex(v);
@@ -120,61 +118,59 @@ def lpa_star(problem, start_loc, goal_loc,agent,h_values):
 
 
 
+def push_node(open_list, node, priority):
+    # print("pushing node ", node)
+    heapq.heappush(open_list, (priority[0],priority[1],node['loc'], node))
+    # print("this is open " ,open_list)
+
 # def push_node(open_list, node, priority):
-#     # print(open_list[1])
-#     if (priority[0], priority[1],node["loc"],node) not in open_list:
-#         heapq.heappush(open_list, (priority[0], priority[1],node["loc"],node["time"],node))
-#     else:
-#         print("it is in open list ")
-#         update(open_list, node, priority)
+#     loc, time = node["loc"], node["time"]
+#     if (loc, time) in node_map:
+#         existing_node = node_map[(loc, time)]
+#         existing_priority = existing_node["priority"]
 
-def push_node(open_list, node, priority, node_map):
-    loc, time = node["loc"], node["time"]
-    if (loc, time) in node_map:
-        existing_node = node_map[(loc, time)]
-        existing_priority = existing_node["priority"]
+#         # Replace only if the new priority is better
+#         if priority < existing_priority:
+#             remove_node(open_list, existing_node, node_map)
 
-        # Replace only if the new priority is better
-        if priority < existing_priority:
-            remove_node(open_list, existing_node, node_map)
-
-    # Add the node to the map and heap
-    node_map[(loc, time)] = node
-    node["priority"] = priority  # Update the node's priority
-    heapq.heappush(open_list, (priority[0], priority[1], time, node))
+#     # Add the node to the map and heap
+#     node_map[(loc, time)] = node
+#     node["priority"] = priority  # Update the node's priority
+#     heapq.heappush(open_list, (priority[0], priority[1], time, node))
   
 
 def pop_node(open_list):
-    while open_list:
-        _, _, _, node = heapq.heappop(open_list)
-        if node["priority"] != (float('inf'), float('inf')):  # Skip invalid nodes
-            return node
-    return None  # Return None if no valid nodes are left
+    if(len(open_list) == 0):
+        return
+    _, _, _,curr = heapq.heappop(open_list)
+    return curr
+
 
 def top_key(open_list):
-    # print("this is top",open_list[0])
+    print("this is top",open_list[0])
+    print("open list", open_list)
+
     if len(open_list) == 0:
         return (float('inf'), float('inf'))
     return open_list[0]
 
-def remove_node(open_list, node, node_map):
-    loc, time = node["loc"], node["time"]
-    if (loc, time) in node_map:
-        del node_map[(loc, time)]
-    # Mark the node as invalid
-    node["priority"] = (float('inf'), float('inf'))
+def remove_node(open_list, node):
+    rem = (float('-inf'), float('-inf'))
+    update(open_list,node,rem)
+    a = pop_node(open_list)
 
-def update(open_list, node, priority, node_map):
-    loc, time = node["loc"], node["time"]
-    if (loc, time) in node_map:
-        existing_node = node_map[(loc, time)]
-        if existing_node["priority"] <= priority:
-            return  # Do nothing if existing priority is better or equal
-        remove_node(open_list, existing_node, node_map)  # Remove the old node
-
-    # Push the updated node with the new priority
-    push_node(open_list, node, priority, node_map)
-
+def update(open_list, node, priority):
+    for index, (p1, p2, loc,item) in enumerate(open_list):
+        if node == item:
+                if (p1,p2) <= priority:
+                    break
+                del open_list[index]
+                open_list.append((priority[0],priority[1],loc, item))
+                heapq.heapify(open_list)
+                break
+    else:
+            print("calling push2")
+            push_node(open_list,node, priority)
 
 def compare_nodes(n1, n2):
     """Return true is n1 is better than n2."""
